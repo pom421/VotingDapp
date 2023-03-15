95adc9684daea40c654ef8abcf8c8f1f64073afa
pom421 <mauguet.po@gmail.com>
2023-03-15 22:20:26 +0100

    feat: remove old files

--- client/src/components/MainContent.jsx
+++ client/src/components/MainContent.jsx
@@ -11,20 +11,6 @@ export const defaultPage = "hero"
 
 export const MainContent = () => {
   const { step } = useMainContext()
-  //   let componentToDisplay = null
-
-  console.log("step", step)
-
-  // eslint-disable-next-line no-constant-condition
-  //   switch (step) {
-  //     case PageIndex.HERO: {
-  //       componentToDisplay = <Hero />
-  //       break
-  //     }
-  //     default: {
-  //       componentToDisplay = <Layout />
-  //     }
-  //   }
 
   return PageIndex[step] || defaultPage
 }
